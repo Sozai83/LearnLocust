e2c022bf243d14bcec1bb292c82f415742768ebd
Sozai83 <chikushi3.227@gmail.com>
2024-08-11 18:17:13 +1000

    Separated user behaviour and run the task sequentially

---
+++ locust_test_http03.py
@@ -0,0 +1,25 @@
+from locust import HttpUser, task, between, SequentialTaskSet
+
+@task()
+class UserBehaviour(SequentialTaskSet):
+    @task
+    def launch_URL(self):
+        self.client.get('/mercurycruise.psp', name = "launch")
+    
+    @task
+    def login(self):
+        login_data = {"action": "process", 
+                      "userName":"quamile1@gmail.clom", 
+                      "password": "qamile", 
+                      "login.x": "41", 
+                      "login.y":"12"}
+        
+        self.client.post('/login.psp', name = "login", data=login_data)
+
+    
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+    host = "http://newtours.demoaut.com"
+
+    task = [UserBehaviour]
